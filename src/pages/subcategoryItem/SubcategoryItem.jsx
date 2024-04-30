0ef5f33267709b47b17d172700626681679d246a
yasin-arft <yasinarftsocial@gmail.com>
2024-04-30 22:40:35 +0600

    subcategory data load error solved

--- src/pages/subcategoryItem/SubcategoryItem.jsx
+++ src/pages/subcategoryItem/SubcategoryItem.jsx
@@ -10,7 +10,7 @@ const SubcategoryItem = () => {
       <h2 className="text-3xl font-semibold text-center my-4">Art & Craft for {subcategoryName} subcategory</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10">
         {
-          loadedData.map(data => <SubcategoryCard key={data._id} data={data} />)
+          loadedData?.map(data => <SubcategoryCard key={data._id} data={data} />)
         }
       </div>
     </div>
